3e60b264ba50301568c8503ec6a83251897e4902
Ivo Branco <ivo.branco@fccn.pt>
2022-09-13 17:20:51 +0100

    Fix open edx csmh configuration
    If we disable the open edx feature ENABLE_CSMH_EXTENDED we also need
    to remove the installed application
    `lms.djangoapps.coursewarehistoryextended` and remove database router
    `StudentModuleHistoryExtendedRouter`.
    FAN-56
    FAN-60

--- roles/openedx_deploy/files/app/nau_production.py
+++ roles/openedx_deploy/files/app/nau_production.py
@@ -3,11 +3,14 @@ Specific overrides to the base prod settings to NAU.
 """
 import sys
 
+# By default load the production settings and not the tutor ones.
 from .production import *
 
-# Adapted from tutor
-# https://github.com/overhangio/tutor/blob/v12.2.0/tutor/templates/apps/openedx/settings/partials/common_all.py#L85-L97
-
+# Nevertheless we still prefer to use the tutor configuration for logging.
+# It removes the syslog base logger.
+#
+# Adapted from tutor:
+#   https://github.com/overhangio/tutor/blob/v12.2.0/tutor/templates/apps/openedx/settings/partials/common_all.py#L85-L97
 # Change syslog-based loggers which don't work inside docker containers
 LOGGING["handlers"]["local"] = {
     "class": "logging.StreamHandler",
@@ -26,3 +29,21 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="newrelic.console")
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="lms.djangoapps.course_wiki.plugins.markdownedx.wiki_plugin")
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="wiki.plugins.links.wiki_plugin")
+
+
+# When we disable this feature, `FEATURES['ENABLE_CSMH_EXTENDED']`, 
+# we need to remove the next installed app and its database router.
+# If we disable it, we are not using the `csmh` database.
+#
+# Reference:
+#   https://github.com/openedx/edx-platform/blob/9d349a8ecd95443fa22fd3503978e66e5d770e10/lms/envs/common.py#L732-L735
+# 
+# Code adapted from:
+#   https://github.com/overhangio/tutor/blob/3f1dd832e49f58a664e62a633ae94c0339727b10/tutor/templates/apps/openedx/settings/partials/common_all.py#L59-L64
+# 
+if not FEATURES['ENABLE_CSMH_EXTENDED']:
+    # Get rid completely of coursewarehistoryextended, as we do not use the CSMH database
+    INSTALLED_APPS.remove("lms.djangoapps.coursewarehistoryextended")
+    DATABASE_ROUTERS.remove(
+        "openedx.core.lib.django_courseware_routers.StudentModuleHistoryExtendedRouter"
+    )
