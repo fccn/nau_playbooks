73a3425ad796a0b697538531659d737676d43531
Ivo Branco <ivo.branco@fccn.pt>
2023-07-17 12:19:51 +0100

    WIP tutor aspects

---
+++ roles/openedx_tutor_aspects_docker_deploy/README.md
@@ -0,0 +1,6 @@
+# 
+
+## Required variables
+
+- `TUTOR_MYSQL_HOST`
+- `TUTOR_OPENEDX_MYSQL_PASSWORD`
---
+++ roles/openedx_tutor_aspects_docker_deploy/templates/aspects.sh
@@ -0,0 +1,10 @@
+
+# Inspire on:
+# https://github.com/fccn/nau-tutor-configs/blob/main/bin/run
+
+# - tvm
+# - init tvm
+# - tvm activate
+# - install tutor plugins for aspects
+# - enable tutor plugins
+# - tutor local ...
