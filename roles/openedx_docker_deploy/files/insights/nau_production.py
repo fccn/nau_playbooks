1bae01a68b709dfc6c3fc7f2054b3472d26b518a
Ivo Branco <ivo.branco@fccn.pt>
2025-08-05 16:03:11 +0100

    refactor: code clean
    Delete not used deployment of openedx app layer.
    This service has been migrated to Kubernetes.
    
    fccn/nau-technical#591

--- roles/openedx_docker_deploy/files/insights/nau_production.py
+++
@@ -1,7 +0,0 @@
-from .production import *
-
-# Get rid of the "local" handler
-LOGGING["handlers"].pop("local")
-for logger in LOGGING["loggers"].values():
-    if "local" in logger["handlers"]:
-        logger["handlers"].remove("local")
